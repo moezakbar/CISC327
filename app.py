4e8af065f46d8b096f074a8f53c0c07fefb0e1f2
moezakbar <moezakbar@hotmail.com>
2023-10-20 00:24:30 -0400

    Added demo front page for the delivery app

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-print("hello world!")
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def front_page():
+    return render_template('front_page.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
