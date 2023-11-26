958fc940d68d8af7b31a54cc5e7c29d041659432
moezakbar <moezakbar@hotmail.com>
2023-11-26 00:15:09 -0500

    statement coverage tests for add restaurant item

---
+++ test_folder_A5/test_add_restaurant_item.py
@@ -0,0 +1,32 @@
+import pytest
+import sys
+import os
+
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from app import app  # Import your Flask application
+from unittest.mock import patch, MagicMock
+
+@pytest.fixture
+def client():
+    with app.test_client() as client:
+        yield client
+
+def test_add_item_get_request(client):
+    # Test GET request
+    response = client.get('/addItem/1')  # Assuming 1 is a valid restaurant owner ID
+    assert response.status_code == 200
+
+
+@patch('app.db')
+def test_add_item_post_success(mock_cursor, client):
+    # Mock database cursor for successful insertion
+    mock_cursor_instance = mock_cursor.return_value
+    mock_cursor_instance.__enter__.return_value.execute.return_value = None
+
+    # Simulate POST request with valid data
+    data = {'name': 'New Dish', 'price': '15.99', 'image_url': 'https://www.allrecipes.com/thmb/GvGzAzmqmTiCFP9AIisrHZav_Gw=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Copycat-Burger-and-Fries-2000-b09140d301434155bda5a8c8a40f5e95.jpeg', 'restaurant_id_fk': 1}
+    response = client.post('/add_to_cart', data={'item_id': 1, 'restaurant_id': 1, 'user_id': 1})
+    assert response.status_code == 302
+  
